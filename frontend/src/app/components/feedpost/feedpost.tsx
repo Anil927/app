3b6e8af02368d5fe5e78efc2ba7283fb00da9883
Anil927 <anilpatra927@gmail.com>
2024-06-09 17:00:29 +0530

    bottom tab height issue fixed

--- frontend/src/app/components/feedpost/feedpost.tsx
+++ frontend/src/app/components/feedpost/feedpost.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import './feedpost.css'
 import CommentModal from '../bottomsheet/bottomsheet'
+import Image from 'next/image'
 
 interface Props {
     postId: number,
@@ -52,7 +53,7 @@ const FeedPost: React.FC<Props> = ({
         setIsCommentModalOpen(true);
         document.body.style.overflow = 'hidden';  // Disable scrolling
     };
-    
+
     const closeCommentModal = () => {
         handleLikeCommentSaveClick('comment');
         setIsCommentModalOpen(false);
@@ -115,15 +116,21 @@ const FeedPost: React.FC<Props> = ({
                             <div>{noOfLikes + likeCommentSaveCount[0]}</div>
                         </button>
                         <button onClick={openCommentModal}>
-                            {
+                            {/* {
                                 likeCommentSaveCount[1] > noOfComments ?
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#20a7db" d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" /></svg>
                                     :
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M160 368c26.5 0 48 21.5 48 48v16l72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6H448c8.8 0 16-7.2 16-16V64c0-8.8-7.2-16-16-16H64c-8.8 0-16 7.2-16 16V352c0 8.8 7.2 16 16 16h96zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3V474.7v-6.4V468v-4V416H112 64c-35.3 0-64-28.7-64-64V64C0 28.7 28.7 0 64 0H448c35.3 0 64 28.7 64 64V352c0 35.3-28.7 64-64 64H309.3L208 492z" /></svg>
                             }
-                            <div>{likeCommentSaveCount[1]}</div></button>
+                            <div>{likeCommentSaveCount[1]}</div> */}
+                            <Image src="./home/comment.svg" alt="comment" width={16} height={16} /> <div>2.4k</div>
+                        </button>
                         {isCommentModalOpen && <CommentModal callback={handleCommentCallback} postId={postId} noOfComments={likeCommentSaveCount[1]} onClose={closeCommentModal} />}
-                        <button onClick={handleShareClick}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M400 255.4V240 208c0-8.8-7.2-16-16-16H352 336 289.5c-50.9 0-93.9 33.5-108.3 79.6c-3.3-9.4-5.2-19.8-5.2-31.6c0-61.9 50.1-112 112-112h48 16 32c8.8 0 16-7.2 16-16V80 64.6L506 160 400 255.4zM336 240h16v48c0 17.7 14.3 32 32 32h3.7c7.9 0 15.5-2.9 21.4-8.2l139-125.1c7.6-6.8 11.9-16.5 11.9-26.7s-4.3-19.9-11.9-26.7L409.9 8.9C403.5 3.2 395.3 0 386.7 0C367.5 0 352 15.5 352 34.7V80H336 304 288c-88.4 0-160 71.6-160 160c0 60.4 34.6 99.1 63.9 120.9c5.9 4.4 11.5 8.1 16.7 11.2c4.4 2.7 8.5 4.9 11.9 6.6c3.4 1.7 6.2 3 8.2 3.9c2.2 1 4.6 1.4 7.1 1.4h2.5c9.8 0 17.8-8 17.8-17.8c0-7.8-5.3-14.7-11.6-19.5l0 0c-.4-.3-.7-.5-1.1-.8c-1.7-1.1-3.4-2.5-5-4.1c-.8-.8-1.7-1.6-2.5-2.6s-1.6-1.9-2.4-2.9c-1.8-2.5-3.5-5.3-5-8.5c-2.6-6-4.3-13.3-4.3-22.4c0-36.1 29.3-65.5 65.5-65.5H304h32zM72 32C32.2 32 0 64.2 0 104V440c0 39.8 32.2 72 72 72H408c39.8 0 72-32.2 72-72V376c0-13.3-10.7-24-24-24s-24 10.7-24 24v64c0 13.3-10.7 24-24 24H72c-13.3 0-24-10.7-24-24V104c0-13.3 10.7-24 24-24h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H72z" /></svg><div>Share</div></button>
+                        <button onClick={handleShareClick}>
+                            {/*<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                             <path d="M400 255.4V240 208c0-8.8-7.2-16-16-16H352 336 289.5c-50.9 0-93.9 33.5-108.3 79.6c-3.3-9.4-5.2-19.8-5.2-31.6c0-61.9 50.1-112 112-112h48 16 32c8.8 0 16-7.2 16-16V80 64.6L506 160 400 255.4zM336 240h16v48c0 17.7 14.3 32 32 32h3.7c7.9 0 15.5-2.9 21.4-8.2l139-125.1c7.6-6.8 11.9-16.5 11.9-26.7s-4.3-19.9-11.9-26.7L409.9 8.9C403.5 3.2 395.3 0 386.7 0C367.5 0 352 15.5 352 34.7V80H336 304 288c-88.4 0-160 71.6-160 160c0 60.4 34.6 99.1 63.9 120.9c5.9 4.4 11.5 8.1 16.7 11.2c4.4 2.7 8.5 4.9 11.9 6.6c3.4 1.7 6.2 3 8.2 3.9c2.2 1 4.6 1.4 7.1 1.4h2.5c9.8 0 17.8-8 17.8-17.8c0-7.8-5.3-14.7-11.6-19.5l0 0c-.4-.3-.7-.5-1.1-.8c-1.7-1.1-3.4-2.5-5-4.1c-.8-.8-1.7-1.6-2.5-2.6s-1.6-1.9-2.4-2.9c-1.8-2.5-3.5-5.3-5-8.5c-2.6-6-4.3-13.3-4.3-22.4c0-36.1 29.3-65.5 65.5-65.5H304h32zM72 32C32.2 32 0 64.2 0 104V440c0 39.8 32.2 72 72 72H408c39.8 0 72-32.2 72-72V376c0-13.3-10.7-24-24-24s-24 10.7-24 24v64c0 13.3-10.7 24-24 24H72c-13.3 0-24-10.7-24-24V104c0-13.3 10.7-24 24-24h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H72z" /></svg><div>Share</div> */}
+                            <Image src="./home/share.svg" alt="comment" width={16} height={16} /> <div>38</div>
+                        </button>
                     </div>
                     <div className='feed-post-content-right'>
                         <button onClick={() => handleLikeCommentSaveClick('save')}>
